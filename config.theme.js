42d2e58e138bee3fd28f184ee5c895fc57b3c7a6
marceloTLD <marcelo.tld.snchez@gmail.com>
2022-12-10 20:12:45 -0300

    Versión inicial

---
+++ config.theme.js
@@ -0,0 +1,13 @@
+const cfgPurge = {
+  content: ['./**/*.ejs', './**/*.html']
+}
+
+module.exports = {
+  // Theme information
+  themeName: "BloggerBase",
+  themeUri: "#url_goes_here",
+  themeVersion: "0.1.0",
+  themeLicense: "MIT license",
+  themeAuthor: "MarceloTLD (Copyright karasu-themes 2020-2022)",
+  themeAuthorUri: "https://github.com/Karasu-themes/BloggerBase"
+}
