082df3136973a4af70d83a1a3cfba185f79e245c
marceloTLD <marcelo.tld.snchez@gmail.com>
2022-12-24 23:49:49 -0300

    chore: Se agregó la licencia correcta

--- config.theme.js
+++ config.theme.js
@@ -7,7 +7,7 @@ module.exports = {
   themeName: "BloggerBase",
   themeUri: "#url_goes_here",
   themeVersion: "0.1.0",
-  themeLicense: "MIT license",
+  themeLicense: "Release under license GPL 3.0",
   themeAuthor: "MarceloTLD (Copyright karasu-themes 2020-2022)",
   themeAuthorUri: "https://github.com/Karasu-themes/BloggerBase"
 }
