7c4cfa6a9497e188f53df1b997f5d3383938728e
MarceloTLD <marcelo.tld.snchez@gmail.com>
2024-02-09 16:16:22 -0300

    Se han eliminado archivos obsoletos para la v2

--- theme.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  themeName: "BloggerBase",
-  themeDescription: "A blank template for build a blogger template",
-  themeUrl: "https://github.com/Karasu-themes/BloggerBase/tree/main/bundle",
-  themeAuthor: "MarceloTLD",
-  themeAuthorUrl: "https://github.com/marceloTLD",
-  themeVersion: "1.0.3",
-  themeLicense: "Release under MIT License"
-}
