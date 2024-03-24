5b3d47265d522e24a9753a1b52f632bb72537ed6
MarceloTLD <marcelo.tld.snchez@gmail.com>
2024-03-24 17:27:51 -0300

    Formateo de algunas líneas

--- core/script.js
+++ core/script.js
@@ -5,7 +5,7 @@ import { getRollupConfig, btagSelector, btagParams } from './utils.js';
 const ROOT_ASSET = path.resolve('./app/assets/');
 
 export default async function scripts(content) {
-  let html=content;
+  let html = content;
   const rollupConfig = await getRollupConfig(process.env.PWD);
   const bscripts = btagSelector(content, 'b:script');
 
@@ -17,13 +17,13 @@ export default async function scripts(content) {
       plugins: [...rollupConfig.plugins]
     });
 
-    const { output } = await bundle.generate({ 
-      format: params.format || "iife", 
-      name: params.name ?? path.basename(pathFile).replace('.js', '') 
+    const { output } = await bundle.generate({
+      format: params.format || "iife",
+      name: params.name ?? path.basename(pathFile).replace('.js', '')
     });
     const [{ code }] = output;
 
-    html=html.replace(bscript, `<script>\n${params.cdta ? `//<![CDATA[\n${code.trim()}\n//]]>` : code.trim()}</script>`)
+    html = html.replace(bscript, `<script>\n${params.cdta ? `//<![CDATA[\n${code.trim()}\n//]]>` : code.trim()}</script>`)
   }
 
   return html;
--- core/styles.js
+++ core/styles.js
@@ -4,32 +4,32 @@ import { cssBuild } from './style-directive.js';
 import { extname } from 'node:path';
 
 export default async function styles(content) {
-  let html=content;
+  let html = content;
   const postcssConfig = await getPostcssConfig(process.env.PWD);
   const bstyles = btagSelector(content, 'b:style');
-  
+
   for (const bstyle of bstyles) {
     const params = btagParams(bstyle);
-    let compiled='';
+    let compiled = '';
 
-      const lang = extname(params.src).replace('.', '');
+    const lang = extname(params.src).replace('.', '');
 
-      // Avisamos que una etiqueta b:style no tiene el atributo src para funcionar
-      if (!params.src) { console.log(`${chalk.yellowBright(`[warning]: An ${chalk.bold("<b:style>")} tag does not have the src (required) attribute`)}`); }
+    // Avisamos que una etiqueta b:style no tiene el atributo src para funcionar
+    if (!params.src) { console.log(`${chalk.yellowBright(`[warning]: An ${chalk.bold("<b:style>")} tag does not have the src (required) attribute`)}`); }
 
-      if (params.src) {
-        compiled = await cssBuild(params.src, {
-          cdta: params.cdta,
-          lang: lang ?? "css",
-          style: params.output || "compressed",
-          plugins: postcssConfig.plugins
-        });
-      }
+    if (params.src) {
+      compiled = await cssBuild(params.src, {
+        cdta: params.cdta,
+        lang: lang ?? "css",
+        style: params.output || "compressed",
+        plugins: postcssConfig.plugins
+      });
+    }
 
     if (params.render && params.src) {
-      html=html.replace(bstyle, `${tagStyle(params.cdta, compiled.trim())}`);
+      html = html.replace(bstyle, `${tagStyle(params.cdta, compiled.trim())}`);
     } else {
-      html=html.replace(bstyle, compiled.trim());
+      html = html.replace(bstyle, compiled.trim());
     }
   }
 
