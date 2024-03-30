013151986ef4c6324ecb08b3e8644d10021e808f
MarceloTLD <marcelo.tld.snchez@gmail.com>
2024-03-30 11:20:04 -0300

    Ahora el archivo de configuración se importa desde un archivo separado

--- bin/index.js
+++ bin/index.js
@@ -1,19 +1,8 @@
 #!/usr/bin/env node
+import { CONFIG } from '../core/config.js';
 import { program } from 'commander';
 import build from './build.js';
 import start from './start.js';
-
-// Get the current path
-const APP_PATH = process.env.PWD;
-
-// Default folder name
-const DEFAULT_FOLDER = 'app';
-
-// Configuration
-const CONFIG = {
-  route: APP_PATH,
-  folderName: DEFAULT_FOLDER
-}
 
 // Run the build mode
 build(program, CONFIG);
---
+++ core/config.js
@@ -0,0 +1,10 @@
+// Get the current path
+const APP_PATH = process.env.PWD;
+
+// Default folder name
+const DEFAULT_FOLDER = 'app';
+
+export const CONFIG = {
+  route: APP_PATH,
+  folderName: DEFAULT_FOLDER
+}
