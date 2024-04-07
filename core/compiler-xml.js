91db7c1f6d60c1b4335402dcbc0a4b6f061769da
MarceloTLD <marcelo.tld.snchez@gmail.com>
2024-04-07 19:54:30 -0400

    Agregado un custom tags para manejar las inclusiones con problemas de indentación

--- core/compiler-xml.js
+++ core/compiler-xml.js
@@ -10,10 +10,37 @@ export default async function compilerXML(opts) {
   const themeOpts = await getThemeConfig(CONFIG.route);
 
   // Set nunjucks configuration
-  nunjucks.configure(path.resolve(path.join(CONFIG.route, CONFIG.folderName)));
+  var env = new nunjucks.Environment(
+    new nunjucks.FileSystemLoader(path.resolve(path.join(CONFIG.route, CONFIG.folderName)))
+  );
+
+  /**
+   * Custom tags: binclude
+   * This custom tag fixes the indentation error caused when including files inside app.{njk,html,xml}
+   * by adding some hacks, we can fix the visual error and return the correct indentation level.
+   */
+  function BloggerInclude() {
+    this.tags = ['binclude'];
+
+    this.parse = function (parser, nodes) {
+      const tok = parser.nextToken();
+
+      const args = parser.parseSignature(null, true);
+      parser.advanceAfterBlockEnd(tok.value);
+
+      return new nodes.CallExtension(this, 'run', args);
+    }
+
+    this.run = function (context, url) {
+      const content = env.render(url);
+      return new nunjucks.runtime.SafeString(content.replace(/\n/g, '\n' + ' '.repeat(6)));
+    }
+  }
+
+  env.addExtension('BloggerInclude', new BloggerInclude());
 
   // Render nunjucks files
-  const njkContent = nunjucks.render(entryPointPath, {
+  const njkContent = env.render(entryPointPath, {
     ...njkOpts,
     ...opts,
     ...themeOpts
