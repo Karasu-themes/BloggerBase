32a9d18164b26daee72d93b33d0bdb0cf1a973e6
MarceloTLD <marcelo.tld.snchez@gmail.com>
2024-04-09 22:53:08 -0400

    Se ha corregido un problema con la función addAutoIDWidget
    Esta capturaba de manera incorrecta los diferentes tipos de widgets disponibles

--- core/blogger-directives.js
+++ core/blogger-directives.js
@@ -1,3 +1,5 @@
+import { btagParams } from './utils.js';
+
 export function addAutoIDWidget(content) {
   let compiled = content;
   const bwidgets = content.match(/<b\:widget(.*)?>/g) ?? [];
@@ -36,9 +38,17 @@ export function addAutoIDWidget(content) {
     for (const bwidget of bwidgets) {
       const wtype = /type=['"][A-Za-z-0-9]+['"]/.exec(bwidget);
       const ctype = wtype[0].replace(/(?:\w+=|')/g, '');
+      const isAutoCloseTag = /\/>/g.test(bwidget) ?? false;
+
       if (ctype == type && !bwidget.includes('id=')) {
+        let attrs = '';
+        const params = btagParams(wtype.input);
         count++;
-        compiled = compiled.replace(bwidget, `<b:widget type='${type}' id='${type}${count}' />`)
+
+        params.id = type + count;
+        for (const key in params) attrs += `${key}="${params[key]}" `;
+
+        compiled = compiled.replace(bwidget, `<b:widget ${attrs}${(isAutoCloseTag ? "/" : "")}>`)
       }
     }
 
